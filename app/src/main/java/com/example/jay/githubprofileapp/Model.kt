e612926fab7f5a47c7ea8a32c3c4027d412032bf
parth222 <parth222@users.noreply.github.com>
2019-12-31 21:58:27 +0530

    done fetch all information of user (#5)
    * done fetch all information of user
    
    * done removingn unwanted file

---
+++ app/src/main/java/com/example/jay/githubprofileapp/Model.kt
@@ -0,0 +1,9 @@
+package com.example.jay.githubprofileapp
+
+object Model {
+
+    data class Result(val login: String,
+                      val bio:String,
+                      val url:String)
+
+}
